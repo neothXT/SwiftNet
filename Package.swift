e106fbfeb7bd57deba95b94a7ade1be95f9ced8a
Maciej Burdzicki <maciej.burdzicki@icloud.com>
2022-03-25 19:26:23 +0100

    Package update

--- Package.swift
+++ Package.swift
@@ -25,7 +25,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CombineNetworking",
-            dependencies: ["KeychainAccess"]),
+            dependencies: ["KeychainAccess"],
+			exclude: ["../../CombineNetworking.podspec"]),
         .testTarget(
             name: "CombineNetworkingTests",
             dependencies: ["CombineNetworking"]),
